c8ea1094cc99dbf6ec75a1d428ce14e9f18c58d9
joda <yorgy629@hotmail.com>
2020-04-12 22:53:34 -0500

    listo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let boxCard= document.getElementById("box"),
     logoCar= document.getElementById("car"),
     close=document.getElementById("close"),
-    add= document.querySelector(".add"),
+    add= document.getElementsByClassName("info__details"),
     c=console.log
 
     c(add)
@@ -20,22 +20,57 @@ close.addEventListener("click", ()=>
     boxCard.style.display="none";
 })
 
-add.addEventListener("click", e =>
+
+for(let i =0 ; i< add.length;i++)
 {
-    let info=e.target.parentElement.parentElement.parentElement
-
-
-    let data=
+    if(add[i].addEventListener("click",function(e)
     {
-        title : info.children[0].children[0].textContent,
-        img   : info.children[0].children[1].src,
-        price : info.children[1].children[1].children[1].textContent
+    
+        let data;
+
+        if(e.target.classList.contains("add"))
+        {
+            let info=e.target.parentElement.parentElement.parentElement
+
+            alert("hey")
+            data=
+            {
+                title : info.children[0].children[0].textContent,
+                img   : info.children[0].children[1].src,
+                price : info.children[1].children[1].children[1].textContent
+            }
+        }
+
+        mostrarInfo(data)     
+    })
+    )
+    {
+
     }
 
+}
 
-    mostrarInfo(data)
 
-})
+
+function mostrarInfo(info)
+{
+
+    let li=document.createElement("li"),
+        ul=document.getElementById("courses")
+
+
+        li.innerHTML+=
+        `
+            <p>${info.title}</p>
+            <img src="${info.img}"  width="50" style="display:block">
+            <span>${info.price}</span>
+            <hr>
+        `
+
+    ul.appendChild(li)
+
+    
+}
 
 
 
